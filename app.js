c1c3039c3432a6be15f4073c46a4bec3bc1f72c2
Tim Wheeler <tim@timwheeler.com>
2018-05-08 00:53:36 +0000

    Lesson 8

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
 const bookList = document.querySelector('#book-list');
 
-//output parent element of #book-list
-console.log('book-list parent element:', bookList.parentElement);
+console.log('#book-list next sibling:', bookList.nextSibling);
+console.log('#book-list next element sibling:', bookList.nextElementSibling);
+console.log('#book-list previous sibling:', bookList.previousSibling);
+console.log('#book-list previous element sibling:', bookList.previousElementSibling);
 
-//output parent node of #book-list
-console.log('book-list parent node:', bookList.parentNode);
-
-//outputs child nodes of #book-list, including line breaks, etc.
-// console.log(bookList.childNodes);
-
-//output child nodes of #book-list without line-breaks, etc.
-console.log(bookList.children);
+bookList.previousElementSibling.querySelector('p').innerHTML += '<br />Too cool for everyone else!';
 
