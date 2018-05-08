a1f027c6c157352dc316be139321247f60b92f32
Tim Wheeler <tim@timwheeler.com>
2018-05-08 00:14:49 +0000

    Lesson 3

--- app.js
+++ app.js
@@ -4,3 +4,20 @@ var banner = document.getElementById('page-banner');
 //select book-list section
 var bookList = document.getElementById('book-list');
 
+//select all title class elements on page
+var titles = document.getElementsByClassName('title')
+
+//select all li's on page
+var lis = document.getElementsByTagName('li')
+
+//check if var titles is an array => returns false
+console.log(Array.isArray(titles))
+
+//convert var titles into an array => returns true
+console.log(Array.isArray(Array.from(titles)));
+
+//log each element in the titles array
+Array.from(titles).forEach(function(title){
+  console.log(title);
+});
+
