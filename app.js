5be6c3a6a3a7a220cf18bc52155e9af8f9903eb2
Tim Wheeler <tim@timwheeler.com>
2018-05-08 00:34:51 +0000

    Lesson 5

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
-// console.log(wmf);
-
-//return single elements
-var books = document.querySelector('#book-list li .name');
-// console.log(books)
-
-//return all elements of this type
-books = document.querySelectorAll('#book-list li .name');
+const books = document.querySelectorAll('#book-list li .name');
 
 Array.from(books).forEach(function(book){
-    console.log(book);
-})
+    console.log(book.textContent += ' (Book title)');
+});
+
+const bookList = document.querySelector('#book-list');
+bookList.innerHTML = '<h2>Books and more books</h2>';
+bookList.innerHTML += '<p>This is how you add HTML</p>'
