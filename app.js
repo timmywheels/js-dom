ba1436a81dc6a74a8a97924eb5b9a5314d534ae5
Tim Wheeler <tim@timwheeler.com>
2018-05-08 03:18:46 +0000

    Lesson 14

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+
+var book = document.querySelector('li:first-child .name')
+
+book.getAttribute('class') //returns name
+book.setAttribute('class', ' name-2') //changes class to name-2
+book.hasAttribute('class') //returns true
+book.hasAttribute('href') //returns false
+book.removeAttribute('class') //removes class attribute
+book.setAttribute('class', 'name') //sets class attribute to 'name'
+
+
 const list = document.querySelector('#book-list ul');
 
 //delete books
