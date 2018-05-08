02bc709cd9dda473f213ee25adce61581f6dfd6b
Tim Wheeler <tim@timwheeler.com>
2018-05-08 02:44:12 +0000

    Lesson 11

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
 const list = document.querySelector('#book-list ul');
 
-//refactoring to use event bubbling for the parent ul
-//instead of adding event listeners to each delete button inside each li
+//delete books using event bubbling
 list.addEventListener('click', function(e){
   if(e.target.className == 'delete'){
     const li = e.target.parentElement;
     list.removeChild(li);
   }
 });
+
+//view forms on the page
+// const forms = document.forms;
+// console.log(forms);
+//view the #add-book form
+// console.log(forms['add-book']);
+
+
+
+//add book-list
+const addForm = document.forms['add-book'];
+
+//get value of add book form field
+addForm.addEventListener('submit', function(e){
+  e.preventDefault();
+  const value = addForm.querySelector('input[type="text"]').value;
+  console.log(value);
+});
+
